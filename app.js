ac01d55842aee82f6dd857d88bee4c967ebee9c5
shrey vernekar <glitchguru03@gmail.com>
2025-01-05 21:52:08 +0530

    learned about multer and how to upload images

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto')
+const fs = require('fs');
+const path = require('path'); 
+const multer = require('multer');
+
+
 
 const port = 3000;
 
@@ -13,9 +19,36 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+      crypto.randomBytes(12, function(err, bytes){
+        const fn = bytes.toString('hex') + path.extname(file.originalname)
+        cb(null, fn)
+      })
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
 //show create account page 
 app.get('/',function(req,res){
     res.render("index");
+})
+
+//show image upload page
+app.get('/imageUpload',function(req,res){
+    res.render("imageUpload");
+})
+
+//perform image upload
+app.post('/upload',upload.single('image'),function(req,res){
+    console.log(req.file);
+    res.redirect('/imageUpload')
 })
 
 //show profile page
