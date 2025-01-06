615b0678bf3c03a501ae3b3d56bcccb916108817
shrey vernekar <glitchguru03@gmail.com>
2025-01-06 22:03:37 +0530

    implimented upload profile pic feature

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto')
-const fs = require('fs');
+const upload = require("./configs/multerconfig");
 const path = require('path'); 
-const multer = require('multer');
-
-
 
 const port = 3000;
 
@@ -18,39 +14,26 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
 
-
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images/uploads')
-    },
-    filename: function (req, file, cb) {
-      crypto.randomBytes(12, function(err, bytes){
-        const fn = bytes.toString('hex') + path.extname(file.originalname)
-        cb(null, fn)
-      })
-    }
-  })
-  
-  const upload = multer({ storage: storage })
 
 //show create account page 
 app.get('/',function(req,res){
     res.render("index");
 })
 
-//show image upload page
-app.get('/imageUpload',function(req,res){
-    res.render("imageUpload");
+//profile upload page
+app.get('/profile/upload',function(req,res){
+    res.render("profileupload");
 })
 
-//perform image upload
-app.post('/upload',upload.single('image'),function(req,res){
-    console.log(req.file);
-    res.redirect('/imageUpload')
+app.post('/upload',isLoggedIn, upload.single("image"),async function(req,res){
+    // console.log(req.file);
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
 })
-
 //show profile page
 app.get('/profile',isLoggedIn,async function(req,res){
     let user = await userModel.findOne({email: req.user.email}).populate("posts");
